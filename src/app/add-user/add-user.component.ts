bf8901a3eeef92c69233fa8b5302b04793eecb18
Ognjen <ozivkovic220rn@raf.rs>
2024-03-10 12:31:31 +0100

    Validation

--- src/app/add-user/add-user.component.ts
+++ src/app/add-user/add-user.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common'; 
+
 
 
 @Component({
   selector: 'app-add-user',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './add-user.component.html',
   styleUrl: './add-user.component.css'
 })
@@ -17,27 +20,71 @@ export class AddUserComponent {
     jmbg: '',
     phone: '',
     position: 'Manager',
-    active: false
+    active: true
   };
 
-  search(){
-    
-  }
-
-
-  onCancelAddUserPopup() {
-
-    // Add logic for cancel button
-  }
+  constructor(private router: Router) {}
 
   onCreateAddUserPopup() {
 
-    // Add logic for create button
+    if (this.validateForm()) {
+      console.log('Uspesno kreiran korisnik:', this.addUserData);
+
+      // this.createUserData(this.addUserData);
+      this.router.navigate(['/user/list']);
+    } else {
+      console.error('Forma nije validna.');
+    }
+  }
+
+  onCancelAddUserPopup() {
+    this.router.navigate(['/user/list']);
   }
 
   onCloseAddUserPopup() {
+    this.router.navigate(['/user/list']);
+  }
 
-    // Add logic for close button (e.g., close the popup)
+  private validateForm(): boolean {
+   
+
+    if (!this.addUserData.email || !this.isValidEmail(this.addUserData.email)) {
+      console.error('Email nije validan.');
+      return false;
+    }
+
+    if (!this.addUserData.name) {
+      console.error('Name nije validan.');
+      return false;
+    }
+
+    if (!this.addUserData.surname) {
+      console.error('Surname nije validan.');
+      return false;
+    }
+
+    if (!this.addUserData.jmbg || !this.isValidJMBG(this.addUserData.jmbg)) {
+      console.error('JMBG nije validan.');
+      return false;
+    }
+
+    if (!this.addUserData.phone || !this.isValidPhoneNumber(this.addUserData.phone)) {
+      console.error('Broj telefona nije validan.');
+      return false;
+    }
+    return true;
+  }
+
+  private isValidEmail(email: string): boolean {
+    return email.includes('@');
+  }
+
+  private isValidJMBG(jmbg: string): boolean {
+    return jmbg.length === 13;
+  }
+
+  private isValidPhoneNumber(phone: string): boolean {
+    return /^\d+$/.test(phone);
   }
 
 }
--- src/app/update-user/update-user.component.ts
+++ src/app/update-user/update-user.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common'; 
+
 
 
 @Component({
   selector: 'app-update-user',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './update-user.component.html',
   styleUrl: './update-user.component.css'
 })
@@ -18,26 +21,76 @@ export class UpdateUserComponent {
     phone: '',
     password: '',
     position: 'Manager',
-    active: false
+    active: true
   };
 
-  search(){
-    
-  }
-
-
-  onCancelUpdateUserPopup() {
-
-    // Add logic for cancel button
-  }
+  constructor(private router: Router) {}
 
   onCreateUpdateUserPopup() {
 
-    // Add logic for create button
+    if (this.validateForm()) {
+      console.log('Uspesno updatovan korisnik:', this.updateUserData);
+      
+      // this.updateUserData(this.updateUserData);
+      this.router.navigate(['/user/list']);
+    } else {
+      console.error('Forma nije validna.');
+    }
+  }
+
+  onCancelUpdateUserPopup() {
+    this.router.navigate(['/user/list']);
   }
 
   onCloseUpdateUserPopup() {
-
-    // Add logic for close button (e.g., close the popup)
+    this.router.navigate(['/user/list']);
   }
+
+  private validateForm(): boolean {
+   
+
+    if (!this.updateUserData.email || !this.isValidEmail(this.updateUserData.email)) {
+      console.error('Email nije validan.');
+      return false;
+    }
+
+    if (!this.updateUserData.name) {
+      console.error('Name nije validan.');
+      return false;
+    }
+
+    if (!this.updateUserData.password) {
+      console.error('Password nije validan.');
+      return false;
+    }
+
+    if (!this.updateUserData.surname) {
+      console.error('Surname nije validan.');
+      return false;
+    }
+
+    if (!this.updateUserData.jmbg || !this.isValidJMBG(this.updateUserData.jmbg)) {
+      console.error('JMBG nije validan.');
+      return false;
+    }
+
+    if (!this.updateUserData.phone || !this.isValidPhoneNumber(this.updateUserData.phone)) {
+      console.error('Broj telefona nije validan.');
+      return false;
+    }
+    return true;
+  }
+
+  private isValidEmail(email: string): boolean {
+    return email.includes('@');
+  }
+
+  private isValidJMBG(jmbg: string): boolean {
+    return jmbg.length === 13;
+  }
+
+  private isValidPhoneNumber(phone: string): boolean {
+    return /^\d+$/.test(phone);
+  }
+
 }
