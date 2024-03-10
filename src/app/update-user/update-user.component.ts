e0eb56d4dc4a820d7191edd7027a3a9abea937a8
Ognjen <ozivkovic220rn@raf.rs>
2024-03-10 11:22:38 +0100

    Created UpdateUserComponent and styled AddUserComponent

--- src/app/update-user/update-user.component.ts
+++ src/app/update-user/update-user.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-update-user',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './update-user.component.html',
   styleUrl: './update-user.component.css'
 })
 export class UpdateUserComponent {
+  updateUserData = {
+    email: '',
+    name: '',
+    surname: '',
+    jmbg: '',
+    phone: '',
+    password: '',
+    position: 'Manager',
+    active: false
+  };
 
+  search(){
+    
+  }
+
+
+  onCancelUpdateUserPopup() {
+
+    // Add logic for cancel button
+  }
+
+  onCreateUpdateUserPopup() {
+
+    // Add logic for create button
+  }
+
+  onCloseUpdateUserPopup() {
+
+    // Add logic for close button (e.g., close the popup)
+  }
 }
