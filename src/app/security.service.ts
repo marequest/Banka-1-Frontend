8d690b0cf8441b632287309dd4adaf223867c8b9
Luka Lazarevic <lazarevicluka99@gmail.com>
2024-03-11 14:28:50 +0100

    security table

--- src/app/security.service.ts
+++ src/app/security.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface UserSecurity {
+  name: string;
+  email: string;
+  jmbg: string;
+  position: string;
+  phone: string;
+  activity: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class SecurityService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+
+  }
+
+  getUsers(): Observable<UserSecurity[]> {
+    return this.http.get<UserSecurity[]>("/assets/securities.json");
+  }
 }
