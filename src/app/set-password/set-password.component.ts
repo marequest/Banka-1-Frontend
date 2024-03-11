6af798f0be7cfbfe7e020b4db41c20abbb53b227
StevanIN32 <sille3220rn@raf.rs>
2024-03-11 20:41:17 +0100

    initialize create-password page

--- src/app/set-password/set-password.component.ts
+++ src/app/set-password/set-password.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-set-password',
   standalone: true,
   imports: [],
   templateUrl: './set-password.component.html',
-  styleUrl: './set-password.component.css'
+  styleUrl: './set-password.component.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class SetPasswordComponent {
-
+  passVisible: boolean = false;
+  confirmPassVisible: boolean = false;
 }
