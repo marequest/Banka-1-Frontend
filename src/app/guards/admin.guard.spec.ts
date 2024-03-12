8bc208805145d8eafbb6287e0526d21fe1300f7f
Milan <milanj1705@gmail.com>
2024-03-13 00:20:14 +0100

    Fix adminGuard tests

--- src/app/guards/admin.guard.spec.ts
+++ src/app/guards/admin.guard.spec.ts
@@ -2,10 +2,10 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
-import {HttpClient, HttpErrorResponse} from '@angular/common/http';
-import { of, throwError } from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import { of } from 'rxjs';
 import {adminGuard} from "./admin.guard";
-import {ActivatedRouteSnapshot, Router, RouterStateSnapshot} from "@angular/router";
+import {ActivatedRouteSnapshot, RouterStateSnapshot} from "@angular/router";
 
 describe('adminGuard', () => {
   let httpMock: HttpClient;
@@ -32,15 +32,19 @@ describe('adminGuard', () => {
   });
 
   it('should return false on successful API call with non-admin response', async () => {
-    spyOn(httpMock, 'get').and.returnValue(of(false));
+    spyOn(httpMock, 'get').and.returnValue(of({
+      "isAdmin": false
+    }));
     spyOn(localStorage, 'getItem').and.returnValue("token");
     const result = await TestBed.runInInjectionContext(() => adminGuard(route, state));
     expect(result).toBeFalsy();
   });
 
   it('should return true on successful API call with admin response', async () => {
-    spyOn(httpMock, 'get').and.returnValue(of(true));
-    spyOn(localStorage, 'getItem').and.returnValue("token");
+    spyOn(httpMock, 'get').and.returnValue(of({
+      "isAdmin": true
+    }));
+    spyOn(localStorage, 'getItem').and.returnValue("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c");
     const result = await TestBed.runInInjectionContext(() => adminGuard(route, state));
     expect(result).toBeTruthy();
   });
