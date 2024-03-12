7c5d5b5de9ab7d4d841e0ae1d00ea8a34463c1b3
Milan <milanj1705@gmail.com>
2024-03-12 01:48:50 +0100

    Add input validation support

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import {z} from "zod";
+import {ValidationService} from "../validation.service";
 @Component({
   selector: 'app-login-page',
   standalone: true,
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
-
+  constructor(private validator: ValidationService) {}
+  loginFormSchema = z.object({
+    email: z.string().email(),
+    password: z.string()
+  })
 }
