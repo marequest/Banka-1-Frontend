1ea77f2aa154b70adfaac8e47310c8bfe607f157
Milan Jovanovic <milanj1705@gmail.com>
2024-03-24 23:41:36 +0100

    Finish page design

--- src/app/activate-account/activate-account.component.ts
+++ src/app/activate-account/activate-account.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgIf, NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-activate-account',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    NgIf
+  ],
   templateUrl: './activate-account.component.html',
   styleUrl: './activate-account.component.css'
 })
 export class ActivateAccountComponent {
+  stage: 1 | 2 | 3;
+
+  constructor() {
+    this.stage = 1;
+  }
+
+  public nextStage() {
+    this.stage++;
+
+    if(this.stage > 3) {
+      this.stage = 3;
+    }
+  }
+  public previousStage() {
+    this.stage--;
+    if(this.stage < 1) {
+      this.stage = 1;
+    }
+  }
 
 }
