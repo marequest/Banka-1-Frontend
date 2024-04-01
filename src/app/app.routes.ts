cf379d254cae9ac02b6ab5ee4951fd2e2fe87ecc
StevanIN32 <sille3220rn@raf.rs>
2024-04-01 23:38:19 +0200

    new loan component & made responsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -29,6 +29,7 @@ import { CustomerGuard } from './guards/customer.guard';
 import {UserDetailComponent} from "./user-detail/user-detail.component";
 import {ActivateAccountComponent} from "./activate-account/activate-account.component";
 import {FutureViewComponent} from "./future-view/future-view.component";
+import {NewLoanComponent} from "./loans/new-loan.component";
 
 
 
@@ -76,6 +77,7 @@ export const routes: Routes = [
   //TODO: Add customer guard
   { path: 'bank-accounts', component: BankAccountsComponent, canActivate: [CustomerGuard]},
   { path: 'cards', component: CardsComponent,canActivate: [CustomerGuard]},
+  { path: 'loans', component: NewLoanComponent, canActivate: [CustomerGuard]},
 
   // { path: 'customer/:customerId', component: UserDetailComponent},
 
---
+++ src/app/loans/new-loan.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loans',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-loan.component.html',
+  styleUrl: './new-loan.component.css'
+})
+export class NewLoanComponent {
+
+}
