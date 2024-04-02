51c05dfb72aedd515f23a738bcdc1c76315ca0f2
StevanIN32 <sille3220rn@raf.rs>
2024-04-02 02:05:55 +0200

    create detailed loan dialog

--- src/app/loans/loan-table/loan-table.component.ts
+++ src/app/loans/loan-table/loan-table.component.ts
@@ -3,6 +3,8 @@ import {DatePipe, NgForOf, PercentPipe} from "@angular/common";
 import {RouterLink} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
 import {Loan} from "../../model/model"
+import {MatDialog} from "@angular/material/dialog";
+import {LoanDetailsComponent} from "../loan-details/loan-details.component";
 
 @Component({
   selector: 'app-loan-table',
@@ -18,12 +20,26 @@ import {Loan} from "../../model/model"
 })
 export class LoanTableComponent implements OnInit {
   loans: Loan[] = [];
-  constructor(protected http: HttpClient) {
+  constructor(protected http: HttpClient, protected matDialog: MatDialog) {
   }
   ngOnInit(): void {
     this.http.get<Loan[]>('assets/loans.json').subscribe(res => {
-      this.loans = res;
+      this.loans = res.map(loan => {
+        loan.maturityDate *= 1000;
+        loan.agreementDate *= 1000;
+        loan.nextInstallmentDate *= 1000;
+        return loan;
+      });
     })
+  }
+
+  openDialog(element: Loan) {
+    this.matDialog.open(LoanDetailsComponent, {
+      width: '30%',
+      height: '80%',
+      panelClass: 'dialog-class',
+      data: element,
+    });
   }
 
 }
--- src/app/loans/new-loan/new-loan.component.ts
+++ src/app/loans/new-loan/new-loan.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {LoanType} from "../../model/model";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-loans',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    NgForOf
   ],
   templateUrl: './new-loan.component.html',
   styleUrl: './new-loan.component.css'
 })
 export class NewLoanComponent {
 
+  readonly loanTypes = Object.values(LoanType);
 }
