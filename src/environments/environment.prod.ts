4ae2636f4d2799de225c83ecba59362cca2f6d95
sudo0rw3ll <75037128+sudo0rw3ll@users.noreply.github.com>
2024-05-31 17:06:00 +0200

    Merge (#199)
    * Changed DockerFile
    
    * Added docker compose and nginx config
    
    * Added production configuration and fileReplacements statement
    
    * Aded E2E functionality
    
    * Changed name of the step
    
    * Tests
    
    * FE
    
    * Refactor
    
    * tests
    
    * Prod build
    
    * Added HTTPS endpoints
    
    ---------
    
    Co-authored-by: vnikolic7821rn <vnikolic7821rn@raf.rs>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    userService: 'http://banka-1-dev.si.raf.edu.rs/user-service',
-    marketService: 'http://banka-1-dev.si.raf.edu.rs/market-service',
-    fetchingService: 'http://banka-1-dev.si.raf.edu.rs/fetching-service',
+    userService: 'https://banka-1-dev.si.raf.edu.rs/user-service',
+    marketService: 'https://banka-1-dev.si.raf.edu.rs/market-service',
+    fetchingService: 'https://banka-1-dev.si.raf.edu.rs/fetching-service',
     shouldUseMockedDataForLegalPersons: true
 }
